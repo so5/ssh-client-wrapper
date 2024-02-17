a5634d1480b448c54acb8d96730197d1eeb89144
Naoyuki Sogo <sogo@longtail-software.co.jp>
2024-02-18 02:58:38 +0900

    send and recv will retry if exit with 12

--- run_test_all.sh
+++ run_test_all.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+function cleanup(){
+  docker-compose rm --stop --force testbed
+}
+
+trap cleanup EXIT
+set -e -o pipefail
+
 docker-compose up testbed -d
 export TEST_HOST=localhost
 export TEST_USER=testuser
@@ -7,27 +15,13 @@ export TEST_PORT=4000
 ssh-keygen -R '[localhost]:4000'
 
 npm run test
-if [ $? -ne 0 ];then
-  echo local test failed
-  exit 1
-fi
+echo local test success
 
 docker-compose run --build --rm tester
-if [ $? -ne 0 ];then
-  echo test on latest ubuntu failed
-  exit 2
-fi
+echo test on latest ubuntu success
 
 docker-compose run --build --rm tester-bullseye
-if [ $? -ne 0 ];then
-  echo test on bullseye failed
-  exit 3
-fi
+echo test on bullseye success
 
 docker-compose run --build --rm tester-buster
-if [ $? -ne 0 ];then
-  echo test on buster failed
-  exit 4
-fi
-
-docker-compose rm --stop --force testbed
+echo test on buster success
